78abb79bc8f839187fbc8dcf0154110a6c14b71e
Anna Kuzina <ftm.rk1@gmail.com>
2018-06-20 23:56:04 +0300

    upload SPD files

---
+++ SPDnet/spdnet_init_afew.py
@@ -0,0 +1,57 @@
+import numpy as np
+from spdnet.utils.my_svd import my_svd
+
+def spdnet_init_afew(seed=123):
+    
+    np.random.seed(seed)
+    
+    opts = {}
+
+    opts['datadim'] = [116, 200, 50, 25]
+    opts['layernum'] = len(opts['datadim']) - 1
+    Winit = [0] * opts['layernum']
+    
+    for iw in range(0, opts['layernum']):
+        A = np.random.random((opts['datadim'][iw], opts['datadim'][iw]))
+        
+        U1, S1, V1 = my_svd(A.dot(A.T))
+        
+        Winit[iw] = U1[:, 0:opts['datadim'][iw + 1]]
+    
+    
+    f = 1. / 100
+    classNum = 2
+    
+    fdim = Winit[iw].shape[1] * Winit[iw].shape[1]
+    
+    theta = f * np.random.random((fdim, classNum))
+    
+    Winit.append(theta)
+    
+    net = {}
+    
+    net['layers'] = []
+    
+    net['layers'].append({'type': 'bfc', 'weight': Winit[0]})
+    
+    net['layers'].append({'type': 'rec'})
+    
+    net['layers'].append({'type': 'bfc', 'weight': Winit[1]})
+    
+    net['layers'].append({'type': 'rec'})
+    
+#    net['layers'].append({'type': 'bfc', 'weight': Winit[2]})
+    
+#    net['layers'].append({'type': 'rec'})
+    
+    net['layers'].append({'type': 'bfc', 'weight': Winit[2]})
+    
+    net['layers'].append({'type': 'log'})
+    
+    net['layers'].append({'type': 'fc', 'weight': Winit[-1]})
+    
+    net['layers'].append({'type': 'softmaxloss'})
+    
+    net['train_mode'] = True
+    
+    return net
